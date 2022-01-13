79776ae19476ac64daadc0babb3c3c41461db742
Hoon Kim <hoon0661@gmail.com>
2022-01-13 15:19:57 -0500

    added HTML Geolocation API, GoogleMap Api

--- src/main/resources/static/basic.js
+++ src/main/resources/static/basic.js
@@ -1,190 +1,252 @@
 let tempUnit;
 let windSpeed;
 let hasError = false;
-$(document).ready(function () {
-  $("#search-button").on("click", function () {
-    $("#loading-spinner").show();
-    const query = $("#zipcode").val();
-    const length = query.length;
-    showData();
-    setTimeout(function () {
-      $("#loading-spinner").hide();
-      if (hasError) {
-        $("#dataset").hide();
-        $("#errorMessage").show();
-      } else {
-        $("#dataset").show();
-        $("#errorMessage").hide();
-      }
-    }, 2000);
-  });
-
-  $("#edit-button").on("click", function () {
-    editUserInfo();
-  });
-
-  $("#edit-link").on("click", function () {
-    $("#errorMessage-edit").hide();
-    $("#search-main").hide();
-    $("#dataset").hide();
-    $("#edit-area").show();
-  });
-
-  $("#loading-spinner").hide();
-  $("#edit-area").hide();
-  $("#dataset").hide();
-  $("#errorMessage").hide();
-  $("#errorMessage-edit").hide();
+let googleMapApiKey;
+let zipcodeFromLocator;
+$.getScript("apikeys.js", function () {
+    googleMapApiKey = getGoogleMapApi();
 });
 
+$(document).ready(function () {
+    getLocation();
+    setTimeout(function () {
+        if (zipcodeFromLocator !== null && typeof zipcodeFromLocator !== 'undefined') {
+            $("#loading-spinner").show();
+            showData();
+            setTimeout(function () {
+                $("#loading-spinner").hide();
+                if (hasError) {
+                    $("#dataset").hide();
+                    $("#errorMessage").show();
+                } else {
+                    $("#dataset").show();
+                    $("#errorMessage").hide();
+                }
+            }, 2000);
+        }
+    }, 500);
+    $("#search-button").on("click", function () {
+        $("#loading-spinner").show();
+        showData();
+        setTimeout(function () {
+            $("#loading-spinner").hide();
+            if (hasError) {
+                $("#dataset").hide();
+                $("#errorMessage").show();
+            } else {
+                $("#dataset").show();
+                $("#errorMessage").hide();
+            }
+        }, 2000);
+    });
+
+    $("#edit-button").on("click", function () {
+        editUserInfo();
+    });
+
+    $("#edit-link").on("click", function () {
+        $("#errorMessage-edit").hide();
+        $("#search-main").hide();
+        $("#dataset").hide();
+        $("#edit-area").show();
+    });
+
+    $("#loading-spinner").hide();
+    $("#edit-area").hide();
+    $("#dataset").hide();
+    $("#errorMessage").hide();
+    $("#errorMessage-edit").hide();
+});
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getZipcode, showGeoLocationError);
+    } else {
+        return;
+    }
+}
+
+function getZipcode(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    $.ajax({
+        type: "GET",
+        url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleMapApiKey}`,
+        success: function (response) {
+            zipcodeFromLocator = response.results[0].address_components[7].long_name;
+        },
+        error: function (response) {
+            alert("Failed to get zipcode.");
+        },
+    });
+}
+
+function showGeoLocationError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            console.log("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.log("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            console.log("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            console.log("An unknown error occurred.");
+            break;
+    }
+}
+
 function editUserInfo() {
-  const id = $("#userid-edit").val();
-  const username = $("#username-edit").val();
-  const email = $("#email-edit").val();
-  const password = $("#password-edit").val();
-  const data = { id, username, email, password };
-  $.ajax({
-    type: "POST",
-    url: `/user/edit`,
-    contentType: "application/json",
-    data: JSON.stringify(data),
-    success: function (response) {
-      console.log("success");
-      window.location.reload();
-    },
-    error: function (response) {
-      if (response.responseJSON && response.responseJSON.message) {
-        $("#errorMessage-edit").text(response.responseJSON.message);
-      } else {
-        $("#errorMessage-edit").text("Unknown Error Occurred.");
-      }
-      $("#errorMessage-edit").show();
-    },
-  });
+    const id = $("#userid-edit").val();
+    const username = $("#username-edit").val();
+    const email = $("#email-edit").val();
+    const password = $("#password-edit").val();
+    const data = {id, username, email, password};
+    $.ajax({
+        type: "POST",
+        url: `/user/edit`,
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: function (response) {
+            alert("Edit Success!");
+            window.location.reload();
+        },
+        error: function (response) {
+            if (response.responseJSON && response.responseJSON.message) {
+                $("#errorMessage-edit").text(response.responseJSON.message);
+            } else {
+                $("#errorMessage-edit").text("Unknown Error Occurred.");
+            }
+            $("#errorMessage-edit").show();
+        },
+    });
 }
 
 function showData() {
-  const query = $("#zipcode").val();
-  const unit = $("#unit option:selected").val();
-  tempUnit = unit === "metric" ? "&#8451;" : "&#8457;";
-  windSpeed = unit === "metric" ? "m/s" : "mph";
+    const query = $("#zipcode").val() || zipcodeFromLocator;
+    const unit = $("#unit option:selected").val();
+    tempUnit = unit === "metric" ? "&#8451;" : "&#8457;";
+    windSpeed = unit === "metric" ? "m/s" : "mph";
 
-  $.ajax({
-    type: "GET",
-    url: `/api/search/oneday?query=${query}&unit=${unit}`,
-    success: function (response) {
-      hasError = false;
-      $("#data-today").empty();
-      const card = generateCardForToday(response);
-      $("#data-today").append(card);
-      showFiveDaysWeather();
-    },
-    error: function (response) {
-      hasError = true;
-      if (response.responseJSON && response.responseJSON.message) {
-        $("#errorMessage").text(response.responseJSON.message);
-      } else {
-        $("#errorMessage").text("Unknown Error Occurred.");
-      }
-    },
-  });
+    $.ajax({
+        type: "GET",
+        url: `/api/search/oneday?query=${query}&unit=${unit}`,
+        success: function (response) {
+            hasError = false;
+            $("#data-today").empty();
+            const card = generateCardForToday(response);
+            $("#data-today").append(card);
+            showFiveDaysWeather(query);
+        },
+        error: function (response) {
+            hasError = true;
+            if (response.responseJSON && response.responseJSON.message) {
+                $("#errorMessage").text(response.responseJSON.message);
+            } else {
+                $("#errorMessage").text("Unknown Error Occurred.");
+            }
+        },
+    });
 }
 
-function showFiveDaysWeather() {
-  const query = $("#zipcode").val();
-  const unit = $("#unit option:selected").val();
-  tempUnit = unit === "metric" ? "&#8451;" : "&#8457;";
-  windSpeed = unit === "metric" ? "m/s" : "mph";
+function showFiveDaysWeather(query) {
+    // const query = $("#zipcode").val();
+    const unit = $("#unit option:selected").val();
+    tempUnit = unit === "metric" ? "&#8451;" : "&#8457;";
+    windSpeed = unit === "metric" ? "m/s" : "mph";
 
-  $.ajax({
-    type: "GET",
-    url: `/api/search/fivedays?query=${query}&unit=${unit}`,
-    success: function (response) {
-      hasError = false;
-      $("#data-fivedays").empty();
-      for (let i = 0; i < response.length; i++) {
-        let card = generateCardForFiveDays(response[i]);
-        $("#data-fivedays").append(card);
-      }
-      showCovidForGlobal();
-    },
-    error: function (response) {
-      hasError = true;
-      if (response.responseJSON && response.responseJSON.message) {
-        $("#errorMessage").text(response.responseJSON.message);
-      } else {
-        $("#errorMessage").text("Unknown Error Occurred.");
-      }
-    },
-  });
+    $.ajax({
+        type: "GET",
+        url: `/api/search/fivedays?query=${query}&unit=${unit}`,
+        success: function (response) {
+            hasError = false;
+            $("#data-fivedays").empty();
+            for (let i = 0; i < response.length; i++) {
+                let card = generateCardForFiveDays(response[i]);
+                $("#data-fivedays").append(card);
+            }
+            showCovidForGlobal(query);
+        },
+        error: function (response) {
+            hasError = true;
+            if (response.responseJSON && response.responseJSON.message) {
+                $("#errorMessage").text(response.responseJSON.message);
+            } else {
+                $("#errorMessage").text("Unknown Error Occurred.");
+            }
+        },
+    });
 }
 
-function showCovidForGlobal() {
-  $("#data-covid").empty();
-  $.ajax({
-    type: "GET",
-    url: `/api/search/covid/global`,
-    success: function (response) {
-      hasError = false;
-      const tempHtml = generateCardForCovidForGlobal(response);
-      $("#data-covid").append(tempHtml);
-      showCovidForNational();
-    },
-    error: function (response) {
-      hasError = true;
-      if (response.responseJSON && response.responseJSON.message) {
-        $("#errorMessage").text(response.responseJSON.message);
-      } else {
-        $("#errorMessage").text("Unknown Error Occurred.");
-      }
-    },
-  });
+function showCovidForGlobal(query) {
+    $("#data-covid").empty();
+    $.ajax({
+        type: "GET",
+        url: `/api/search/covid/global`,
+        success: function (response) {
+            hasError = false;
+            const tempHtml = generateCardForCovidForGlobal(response);
+            $("#data-covid").append(tempHtml);
+            showCovidForNational(query);
+        },
+        error: function (response) {
+            hasError = true;
+            if (response.responseJSON && response.responseJSON.message) {
+                $("#errorMessage").text(response.responseJSON.message);
+            } else {
+                $("#errorMessage").text("Unknown Error Occurred.");
+            }
+        },
+    });
 }
 
-function showCovidForNational() {
-  $.ajax({
-    type: "GET",
-    url: `/api/search/covid/national`,
-    success: function (response) {
-      hasError = false;
-      const tempHtml = generateCardForCovidForNational(response);
-      $("#data-covid").append(tempHtml);
-      showCovidForState();
-    },
-    error: function (response) {
-      hasError = true;
-      if (response.responseJSON && response.responseJSON.message) {
-        $("#errorMessage").text(response.responseJSON.message);
-      } else {
-        $("#errorMessage").text("Unknown Error Occurred.");
-      }
-    },
-  });
+function showCovidForNational(query) {
+    $.ajax({
+        type: "GET",
+        url: `/api/search/covid/national`,
+        success: function (response) {
+            hasError = false;
+            const tempHtml = generateCardForCovidForNational(response);
+            $("#data-covid").append(tempHtml);
+            showCovidForState(query);
+        },
+        error: function (response) {
+            hasError = true;
+            if (response.responseJSON && response.responseJSON.message) {
+                $("#errorMessage").text(response.responseJSON.message);
+            } else {
+                $("#errorMessage").text("Unknown Error Occurred.");
+            }
+        },
+    });
 }
 
-function showCovidForState() {
-  const query = $("#zipcode").val();
-  $.ajax({
-    type: "GET",
-    url: `/api/search/covid/states?query=${query}`,
-    success: function (response) {
-      hasError = false;
-      const tempHtml = generateCardForCovidForState(response);
-      $("#data-covid").append(tempHtml);
-    },
-    error: function (response) {
-      hasError = true;
-      if (response.responseJSON && response.responseJSON.message) {
-        $("#errorMessage").text(response.responseJSON.message);
-      } else {
-        $("#errorMessage").text("Unknown Error Occurred.");
-      }
-    },
-  });
+function showCovidForState(query) {
+    // const query = $("#zipcode").val();
+    $.ajax({
+        type: "GET",
+        url: `/api/search/covid/states?query=${query}`,
+        success: function (response) {
+            hasError = false;
+            const tempHtml = generateCardForCovidForState(response);
+            $("#data-covid").append(tempHtml);
+        },
+        error: function (response) {
+            hasError = true;
+            if (response.responseJSON && response.responseJSON.message) {
+                $("#errorMessage").text(response.responseJSON.message);
+            } else {
+                $("#errorMessage").text("Unknown Error Occurred.");
+            }
+        },
+    });
 }
 
 function generateCardForToday(response) {
-  return `
+    return `
     <div class="card mb-3">
     <div class="row g-0">
       <div class="col-md-6 d-flex justify-content-center align-items-center today-img-area">
@@ -221,7 +283,7 @@ function generateCardForToday(response) {
 }
 
 function generateCardForFiveDays(response) {
-  return `
+    return `
     <div class="card">
         <div class="d-flex align-items-center">
             <img src="http://openweathermap.org/img/w/${response.icon}.png" class="card-img-top" alt="..." />
@@ -246,7 +308,7 @@ function generateCardForFiveDays(response) {
 }
 
 function generateCardForCovidForGlobal(response) {
-  return `
+    return `
   <div class="card">
             <div class="card-body">
               <p class="card-text text-center h6">
@@ -279,7 +341,7 @@ function generateCardForCovidForGlobal(response) {
 }
 
 function generateCardForCovidForNational(response) {
-  return `
+    return `
   <div class="card">
     <div class="card-body">
       <p class="card-text text-center h6">
@@ -312,7 +374,7 @@ function generateCardForCovidForNational(response) {
 }
 
 function generateCardForCovidForState(response) {
-  return `
+    return `
     <div class="card">
       <div class="card-body">
         <p class="card-text text-center h6">
